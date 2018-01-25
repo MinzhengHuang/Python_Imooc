9216b4a53e8b0a715115e8c182977a41f2fec88f
黄敏争 <mzhuang@storemax.cn>
2018-01-25 13:29:24 +0800

    第三章学习

---
+++ Python_ Rudiment/chapter_3/chapter_3.02.py
@@ -0,0 +1,21 @@
+# Python之print语句
+
+# print语句可以向屏幕上输出指定的文字。比如输出'hello, world'，用代码实现如下：
+#    >>> print 'hello, world'
+# 注意：
+# 1.当我们在Python交互式环境下编写代码时，>>>是Python解释器的提示符，不是代码的一部分。
+# 2.当我们在文本编辑器中编写代码时，千万不要自己添加 >>>。
+
+# print语句也可以跟上多个字符串，用逗号“,”隔开，就可以连成一串输出：
+# >>> print 'The quick brown fox', 'jumps over', 'the lazy dog'
+# The quick brown fox jumps over the lazy dog
+
+# print也可以打印整数，或者计算结果：
+# >>> print 300
+# 300    #运行结果
+# >>> print 100 + 200
+# 300    #运行结果
+
+#input code
+print 'hello, python'
+print 'hello,', 'python'
---
+++ Python_ Rudiment/chapter_3/chapter_3.03.py
@@ -0,0 +1,5 @@
+# Python的注释
+
+# Python的注释以 # 开头，后面的文字直到行尾都算注释
+
+# print 'hello'
