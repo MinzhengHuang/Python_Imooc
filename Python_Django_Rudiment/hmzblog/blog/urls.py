355149a883dbdc6dba7317fdc31506432600996a
黄敏争 <mzhuang@storemax.cn>
2018-02-28 18:06:27 +0800

    format code

--- Python_Django_Rudiment/hmzblog/blog/urls.py
+++ Python_Django_Rudiment/hmzblog/blog/urls.py
@@ -2,14 +2,10 @@ from django.urls import path
 from django.conf.urls import url
 from . import views
 
-# urlpatterns = [
-#     path('index/', views.index),
-# ]
-
 app_name = 'article_page'
 
 urlpatterns = [
-    url(r'^hello/$', views.hello),
+    path('hello/', views.hello),
     url(r'^index/$', views.index),
     url(r'^article/(?P<article_id>[0-9])$', views.article_page, name='article_page'),
 ]
