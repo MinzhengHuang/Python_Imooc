867568a86ff9b342e028d282cdff6e63a87b3e86
黄敏争 <mzhuang@storemax.cn>
2018-03-01 11:05:48 +0800

    第9章 补充内容

--- Python_Django_Rudiment/hmzblog/blog/admin.py
+++ Python_Django_Rudiment/hmzblog/blog/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Artical
 
-admin.site.register(Artical)
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'pub_time')
+    list_filter = ('pub_time',)
+
+
+admin.site.register(Artical, ArticleAdmin)
