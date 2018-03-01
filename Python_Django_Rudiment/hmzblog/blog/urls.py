300722fc1a6baf4762ee6b39e077ddf52c20faff
黄敏争 <mzhuang@storemax.cn>
2018-03-01 10:22:21 +0800

    第8章 完善博客

--- Python_Django_Rudiment/hmzblog/blog/urls.py
+++ Python_Django_Rudiment/hmzblog/blog/urls.py
@@ -7,5 +7,7 @@ app_name = 'article_page'
 urlpatterns = [
     path('hello/', views.hello),
     url(r'^index/$', views.index),
-    url(r'^article/(?P<article_id>[0-9])$', views.article_page, name='article_page'),
+    url(r'^article/(?P<article_id>[0-9]+)$', views.article_page, name='article_page'),
+    url(r'^edit/(?P<article_id>[0-9]+)$', views.edit_page, name='edit_page'),
+    url(r'^edit/action$', views.edit_action, name='edit_action'),
 ]
--- Python_Django_Rudiment/hmzblog/blog/views.py
+++ Python_Django_Rudiment/hmzblog/blog/views.py
@@ -6,6 +6,7 @@ from . import models
 def hello(request):
     return HttpResponse("Hello, Hmz!")
 
+
 # def index(request):
 #     return render(request, 'blog/index.html', {'hello': 'hello,hmz blog haha'})
 
@@ -22,3 +23,26 @@ def index(request):
 def article_page(request, article_id):
     art = models.Artical.objects.get(pk=article_id)
     return render(request, 'blog/article_page.html', {'article': art})
+
+
+def edit_page(request, article_id):
+    if str(article_id) == '0':
+        return render(request, 'blog/edit_page.html')
+    article = models.Artical.objects.get(pk=article_id)
+    return render(request, 'blog/edit_page.html', {'article': article})
+
+
+def edit_action(request):
+    title = request.POST.get('title', 'TITLE')
+    content = request.POST.get('content', 'CONTENT')
+    article_id = request.POST.get('article_id', 0)
+    if article_id == '0':
+        models.Artical.objects.create(title=title, content=content)
+        arts = models.Artical.objects.all()
+        return render(request, 'blog/index.html', {'articles': arts})
+
+    article = models.Artical.objects.get(pk=article_id)
+    article.title = title
+    article.content = content
+    article.save()
+    return render(request, 'blog/article_page.html', {'article': article})
